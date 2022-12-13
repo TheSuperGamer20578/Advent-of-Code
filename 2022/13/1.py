7030ebc3439d9145c4c649fc5406d47cc5958308
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2022-12-13 16:52:37 +1100

    2022 Day 13

---
+++ 2022/13/1.py
@@ -0,0 +1,41 @@
+"""https://adventofcode.com/2022/day/13"""
+from typing import Any
+
+from aoc import *
+
+
+def parse(input: str) -> Any:
+    """Parse the input"""
+    return [[eval(packet) for packet in pair.splitlines()] for pair in input.split("\n\n")]
+
+
+def compare(a: list[int | list], b: list[int | list]) -> bool | None:
+    for left, right in zip(a, b):
+        if isinstance(left, int) and isinstance(right, int):
+            if left < right:
+                return True
+            if left > right:
+                return False
+        elif isinstance(left, list) and isinstance(right, list):
+            comp = compare(left, right)
+            if comp is not None:
+                return comp
+        elif isinstance(left, int):
+            comp = compare([left], right)
+            if comp is not None:
+                return comp
+        else:
+            comp = compare(left, [right])
+            if comp is not None:
+                return comp
+    return len(a) < len(b) if len(a) != len(b) else None
+
+
+def solve(input: Any) -> int | str | Answer:
+    """Solve the puzzle"""
+    return sum(i for i, (a, b) in enumerate(input, 1) if compare(a, b))
+
+
+if __name__ == "__main__":
+    from aoc.run import run
+    run(parse, solve)
---
+++ 2022/13/2.py
@@ -0,0 +1,58 @@
+"""https://adventofcode.com/2022/day/13#part2"""
+from typing import Any
+from dataclasses import dataclass
+
+from aoc import *
+
+
+def compare(a: list[int | list], b: list[int | list]) -> bool | None:
+    for left, right in zip(a, b):
+        if isinstance(left, int) and isinstance(right, int):
+            if left < right:
+                return True
+            if left > right:
+                return False
+        elif isinstance(left, list) and isinstance(right, list):
+            comp = compare(left, right)
+            if comp is not None:
+                return comp
+        elif isinstance(left, int):
+            comp = compare([left], right)
+            if comp is not None:
+                return comp
+        else:
+            comp = compare(left, [right])
+            if comp is not None:
+                return comp
+    return len(a) < len(b) if len(a) != len(b) else None
+
+
+@dataclass
+class Packet:
+    packet: list[list | int]
+
+    def __lt__(self, other: "Packet") -> bool:
+        return compare(self.packet, other.packet)
+    
+    def __eq__(self, other: "Packet") -> bool:
+        return self.packet == other.packet
+
+
+def parse(input: str) -> Any:
+    """Parse the input"""
+    return [Packet(eval(packet)) for packet in input.replace("\n\n", "\n").splitlines()]
+
+
+def solve(input: Any) -> int | str | Answer:
+    """Solve the puzzle"""
+    div1 = Packet([[2]])
+    div2 = Packet([[6]])
+    input.append(div1)
+    input.append(div2)
+    input.sort()
+    return (input.index(div1) + 1) * (input.index(div2) + 1)
+
+
+if __name__ == "__main__":
+    from aoc.run import run
+    run(parse, solve)
